53a39e49ad893776ac95a5c56f4c80d06f938b10
Shankaragouda G <shankaragoudag@shankaroudasmbp.ten.thomsonreuters.com>
2019-04-05 11:04:48 +0530

    Error

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
     console.log('----------------LOGS-----------------\n', log);
     fs.appendFileSync('server.log', log + '\n', (err) => {
         if(err) {
-            console.log('Unable to append to server.log');
+            console.log('Unable to append to server.log file');
         }
     });
     next();
